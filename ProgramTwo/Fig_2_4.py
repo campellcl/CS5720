6e955257c9c6fff5c5c0d99066135ac8da1e19bc
Chris Campell <ccampell.asu@gmail.com>
2018-01-31 21:12:58 -0500

    Tried using line ticks

--- ProgramTwo/Fig_2_4.py
+++ ProgramTwo/Fig_2_4.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import decomposition
 from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.lines as lines
 
 
 def main():
@@ -40,19 +41,24 @@ def main():
 
     # Perform PCA:
     pca = decomposition.PCA(n_components=2)
-    iris_pca = pca.fit_transform(X=X_std, y=None)
+    iris_pca = pca.fit_transform(X=X, y=None)
     # Multiply transformed data by -1 to revert mirror image:
-    # iris_pca = iris_pca * (-1)
+    iris_pca[:,0] = iris_pca[:,0] * (-1)
+
+    # iris_pca[:,1] = iris_pca[:,1] * (-1)
 
     # Plot Results:
     fig, ax = plt.subplots()
+    # verts the distance between
+    y_verts = []
     plt.scatter(x=iris_pca[:,0], y=iris_pca[:,1], marker=(4, 1, 90))
+    plt.scatter(x=[x*-1 for x in X_std[:,0]], y=X_std[:,1], marker=lines.TICKUP, c='green')
     # plt.scatter(x=iris_pca[:,1], y=iris_pca[:,0], marker=(4, 1, 90))
     # Get the difference in position along the y-axis from PCA y-coord and original.
     # y_diff = [x-y for x,y in zip(iris_pca[:,1], iris.values[:,1])]
-    y_diff = [y1-y2 for y1,y2 in zip(iris_pca[:,1], iris.values[:,2])]
+    y_diff = [y1-y2 for y1,y2 in zip(iris_pca[:,1], iris.values[:,1])]
     x_diff = [x-y for x,y in zip(iris_pca[:,0], iris.values[:,0])]
-    plt.errorbar(x=iris_pca[:,0], y=iris_pca[:,1], yerr=y_diff, c='green')
+    # plt.errorbar(x=iris_pca[:,0], y=iris_pca[:,1], yerr=y_diff, c='green')
     plt.title('Principle Components of the Iris Dataset')
     plt.xlabel('Principle Component One')
     plt.ylabel('Principle Component Two')
