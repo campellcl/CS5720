2442ec657ffacc989e8b1747c9d378cda874f539
ccampell <ccampell.asu@gmail.com>
2018-03-01 18:35:17 -0500

    Added pseudocode implementation up to the point where each square has a unique number for each case.

--- ProgramFive/fig_mri_contour.py
+++ ProgramFive/fig_mri_contour.py
@@ -11,6 +11,12 @@ import matplotlib.pyplot as plt
 __author__ = "Chris Campell"
 __version__ = "2/26/2018"
 
+def build_lookup_table(num_bits=4):
+    lookup_table = {}
+    for i in range(2**num_bits - 1):
+        lookup_table['{0:b}'.format(i).zfill(num_bits)] = i
+    return lookup_table
+
 def threshold(isovalue, pgm):
     """
     threshold: Applys a threshold to the 2D feild to make a binary image containing 1 where the data is above the
@@ -26,6 +32,48 @@ def threshold(isovalue, pgm):
                 pgm_thresholded[i, j] = 1
     return pgm_thresholded
 
+def contour(pgm_thresholded, patch=(2,2)):
+    """
+    contour: Forms contour cells based on the provided dimensionality and pgm.
+    :param pgm: A Portable Gray Map (PGM) file.
+    :param patch: The size of the contour cells.
+    :return contour_pgm: The provided PGM converted to contour cells.
+    """
+    contour_pgm = np.zeros(shape=(pgm.shape[0]-1, pgm.shape[1]-1))
+    # First step is to look up the contour lines and put them into the cells.
+    # Build the lookup table index in a clockwise direction by examining neighbors:
+    contour_cells = []
+    for i in range(len(pgm_thresholded) - 1):
+        contour_cell_row = []
+        for j in range(len(pgm_thresholded[i]) - 1):
+            contour_cell = []
+            contour_cell.append((i, j))
+            contour_cell.append((i, j+1))
+            contour_cell.append((i+1, j+1))
+            contour_cell.append((i+1, j))
+            contour_cell_row.append(contour_cell)
+        contour_cells.append(contour_cell_row)
+    contour_cells = np.array(contour_cells)
+
+    # Give every cell a number based on which corners are true/false:
+    for i, contour_cell_row in enumerate(contour_cells):
+        for j, contour_cell in enumerate(contour_cell_row):
+            # Build the lookup-table binary index:
+            cell_bin_index = ''
+            for k, (x, y) in enumerate(contour_cell):
+                # (x, y) is the indices of the contour cell in the original PGM.
+                # Append to the lookup-table binary index:
+                cell_bin_index = cell_bin_index + str(int(pgm_thresholded[x, y]))
+
+            # Give every contour cell a number based on which corners are true/false (via lookup-table):
+            contour_pgm[i, j] = lookup_table[cell_bin_index]
+    pass
+
+
+pgm_str = None
+# This is set in the definition of the algorithm, and is not a magic number: (4 vertex in a square)
+num_bits = 4
+
 with open(sys.argv[1], 'r') as fp:
     pgm_str = fp.read()
     pgm_str = pgm_str.split('\n')
@@ -37,8 +85,13 @@ with open(sys.argv[1], 'r') as fp:
     for i, string_row in enumerate(pgm_str):
         for j, string_col in enumerate(string_row.split(' ')[:-1]):
             pgm[i,j] = int(string_col)
+    lookup_table = build_lookup_table(num_bits=4)
+    pgm_thresholded = threshold(pgm=pgm, isovalue=int(sys.argv[2]))
+    contours = contour(pgm_thresholded)
+
+
     # source: http://scikit-image.org/docs/dev/auto_examples/edges/plot_contours.html
-    contours = measure.find_contours(array=pgm, level=sys.argv[2])
+    # contours = measure.find_contours(array=pgm, level=sys.argv[2])
     fig, ax = plt.subplots()
     cax = ax.imshow(pgm, interpolation='nearest', cmap=plt.cm.gray)
     plt.title('%s, threshold=%s' %(sys.argv[1], sys.argv[2]))
