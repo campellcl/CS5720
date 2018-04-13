730c716d93eeba55b1af50700673f091da982149
ccampell <ccampell.asu@gmail.com>
2018-04-13 18:58:20 -0400

    Tried adding contour lines for 2d circular image; no errors but incorrect lines and position.

--- InClass/weather.py
+++ InClass/weather.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import collections as mc
 from mpl_toolkits.mplot3d import Axes3D
 import copy
 from InClass.open_gl_framework import OpenGLFramework
@@ -115,7 +116,6 @@ def plot_circular_sweeps(sweeps, metadata):
         for i, distances in enumerate(sweep):
             # TODO: Should really be using the azumith in the metadata for angle.
             for angle, distance in enumerate(distances):
-                # TODO: convert to degress before using np.sin and np.cos.
                 x = np.cos(np.radians(angle))*distance
                 y = np.sin(np.radians(angle))*distance
                 x_coords.append(x)
@@ -159,7 +159,6 @@ def plot_circular_sweep_3d(sweep, metadata, sweep_num, threshold=10):
     im = ax.scatter(x_coords_thresholded, y_coords_thresholded, z_coords_thresholded, c=values_thresholded)
     plt.title('Sweep %d 3D' % sweep_num)
     plt.colorbar(mappable=im, ax=ax)
-    # fig.colorbar(im, cax=np.arange(start=np.min(values_thresholded, axis=0), stop=np.max(values_thresholded, axis=0)), orientation='horizontal')
     plt.show()
 
 
@@ -182,6 +181,71 @@ def get_x_y_z_values_from_sweep(sweep, metadata):
     return x_coords, y_coords, z_coords, values
 
 
+def marching_squares_contour(sweep):
+    """
+    marching_squares_contour: Divides the provided sweep data into contour cells and populates with contour lines using
+        the marching squares algorithm.
+    :param sweep: A single doppler radar sweep from a .RFLCTVTY file.
+    :return contours: A list of lists (2d array) containing the start and end points for each contour line segment.
+    """
+    lookup_table = {
+        '0000': None,
+        '0001': [(0, 0.5), (0.5, 1.0)],
+        '0010': [(0.5, 1), (1.0, 0.5)],
+        '0011': [(0, 0.5), (1.0, 0.5)],
+        '0100': [(0.5, 0), (1.0, 0.5)],
+        '0101': [(0, 0.5), (0.5, 0), (0.5, 1.0), (1.0, 0.5)],
+        '0110': [(0.5, 1.0), (0.5, 0)],
+        '0111': [(0, 0.5), (0.5, 0)],
+        '1000': [(0, 0.5), (0.5, 0)],
+        '1001': [(0.5, 1.0), (0.5, 0)],
+        '1010': [(0, 0.5), (0.5, 1.0), (0.5, 0), (1.0, 0.5)],
+        '1011': [(0.5, 0), (1.0, 0.5)],
+        '1100': [(0, 0.5), (1.0, 0.5)],
+        '1101': [(0.5, 1.0), (1.0, 0.5)],
+        '1110': [(0, 0.5), (0.5, 1.0)],
+        '1111': None
+    }
+    contours = []
+    contour_cells = []
+    for i in range(len(sweep) - 1):
+        contour_cell_row = []
+        for j in range(len(sweep[i]) - 1):
+            contour_cell = [(i, j), (i, j+1), (i+1, j+1), (i+1, j)]
+            contour_cell_row.append(contour_cell)
+            # Create a binary index based on the values in the cell:
+            cell_bin_index = ''.join('1' if sweep[x, y] > 0 else '0' for x, y in contour_cell)
+            # Perform a lookup using the binary index:
+            contour_line_segments = lookup_table[cell_bin_index]
+            contour_segment = []
+            if contour_line_segments is not None:
+                # If there are two lines present...
+                if len(contour_line_segments) == 4:
+                    for n, (x, y) in enumerate(contour_line_segments):
+                        contour_segment.append((x+j, y+i))
+                        if n == 1:
+                            contours.append(contour_segment)
+                            contour_segment = []
+                        elif n == 3:
+                            contours.append(contour_segment)
+                else:
+                    for n, (x, y) in enumerate(contour_line_segments):
+                        contour_segment.append((x+j, y+i))
+                    contours.append(contour_segment)
+        contour_cells.append(contour_cell_row)
+    return contours
+
+
+def plot_contour_lines_2d(x_coords, y_coords, values, sweep_num, contour_lines):
+    fig, ax = plt.subplots()
+    plt.scatter(x_coords, y_coords, c=values)
+    lc = mc.LineCollection(contour_lines, linewidths=2, color='red', linestyles='solid')
+    ax.add_collection(lc)
+    plt.title('Sweep %d' % sweep_num)
+    plt.colorbar()
+    plt.show()
+
+
 def main():
     index = 121
     file_name = '../data/weather/%d.RFLCTVTY' % index
@@ -200,11 +264,12 @@ def main():
     # Plot one sweep in 3d:
     # plot_circular_sweep_3d(sweep=sweeps[0], metadata=metadata[0], sweep_num=0)
     x, y, z, values = get_x_y_z_values_from_sweep(sweeps[0], metadata[0])
-    OpenGLFramework(x=x, y=y, z=z, values=values)
-
+    # OpenGLFramework(x=x, y=y, z=z, values=values)
+    contour_lines = marching_squares_contour(sweep=sweeps[0])
+    plot_contour_lines_2d(x_coords=x, y_coords=y, values=values, sweep_num=0, contour_lines=contour_lines)
     # plt.colorbar(mappable=colors)
     # plot_circular_sweeps(sweeps, metadata)
-
+    pass
     # Goal: To create a scatter plot using only angle and distance/radius.
     # How to turn angle and distance into an x and y value?
     # Each sweep has a different angle and height associated with it. The angle is given by the azimuth
